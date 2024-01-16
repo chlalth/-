b053f68101981af1debbeb060334b512ecb226ba
hahehihohu <87600224+HappyDays0921@users.noreply.github.com>
2024-01-16 16:18:16 +0900

    search_3

--- MainActivity.java
+++ MainActivity.java
@@ -1,50 +1,23 @@
-package com.example.searchview;
+package com.example.search;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 
+
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String[] name = {"a","b","c","d","e","f"};
-    ArrayAdapter<String>arrayAdapter;
+    String[] name={"A+","A++","A","A++++","A-","A--"};
+    ArrayAdapter<String> arrayAdapter;
 
 
-
-//
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView)menuItem.getActionView();
-        searchView.setQueryHint("TYPE HERE TO SEARCH");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-
-                arrayAdapter.getFilter().filter(newText);
-
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,5 +26,29 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listview);
         arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,name);
         listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.item1);
+        SearchView searchView = (SearchView)menuItem.getActionView();
+       searchView.setQueryHint("type here to search");
+
+       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+           @Override
+           public boolean onQueryTextSubmit(String query) {
+               return false;
+           }
+
+           @Override
+           public boolean onQueryTextChange(String newText) {
+               return false;
+           }
+       });
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
