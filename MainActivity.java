c1e51dca136e414e9ea2e3044afe6bdb3480c534
hahehihohu <87600224+HappyDays0921@users.noreply.github.com>
2024-01-16 11:16:52 +0900

    search_view

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,57 @@
-package com.example.a0002;
+package com.example.searchview;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.ImageView;
+import androidx.appcompat.widget.SearchView;
 
-import com.bumptech.glide.Glide;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView a;
+
+    ListView listView;
+    String[] name = {"a","b","c","d","e","f"};
+    ArrayAdapter<String>arrayAdapter;
+
+
+
+//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)menuItem.getActionView();
+        searchView.setQueryHint("TYPE HERE TO SEARCH");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                arrayAdapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        a = (ImageView)findViewById(R.id.iv_load_gif);
-        Glide.with(this).load(R.raw.iv_load_gif).into(a);
-
+        listView = findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,name);
+        listView.setAdapter(arrayAdapter);
     }
 }
