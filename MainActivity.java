262dcb6b58503d293a10c9fd121cedc3e518e78d
hahehihohu <87600224+HappyDays0921@users.noreply.github.com>
2024-01-17 16:26:17 +0900

    apple_sale_xml

--- MainActivity.java
+++ MainActivity.java
@@ -1,66 +1,26 @@
-package com.example.search;
+package com.example.a2_main;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 
-
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String[] name={"A+","A++","A","A++++","A-","A--"};
-    ArrayAdapter<String> arrayAdapter;
+    String[] name={"1","2","3","4","5","6","7","8","9","10","11"};
+    ArrayAdapter<String>arrayAdapter;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.p1);
-
-        Button imageButton = (Button)findViewById((R.id.btn1));
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),p2.class);
-                startActivity(intent);
-            }
-        });
-        //listView = findViewById(R.id.listview);
-        //arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,name);
-        //listView.setAdapter(arrayAdapter);
+        setContentView(R.layout.data);
+    listView = findViewById(R.id.listView);
+    arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+    listView.setAdapter(arrayAdapter);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-
-        MenuItem menuItem = menu.findItem(R.id.item1);
-        SearchView searchView = (SearchView)menuItem.getActionView();
-       searchView.setQueryHint("type here to search");
-
-       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-           @Override
-           public boolean onQueryTextSubmit(String query) {
-               return false;
-           }
-
-           @Override
-           public boolean onQueryTextChange(String newText) {
-               return false;
-           }
-       });
-
-        return super.onCreateOptionsMenu(menu);
-
-    }
-
 }
