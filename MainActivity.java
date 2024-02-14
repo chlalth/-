e12194eb6d812d0be63764f585f5d8d8069812c1
jiu702 <137995027+jiu702@users.noreply.github.com>
2024-02-15 00:07:08 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,26 +1,4 @@
-package com.example.a2_main;
+package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-public class MainActivity extends AppCompatActivity {
-
-    ListView listView;
-    String[] name={"1","2","3","4","5","6","7","8","9","10","11"};
-    ArrayAdapter<String>arrayAdapter;
-
-
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.data);
-    listView = findViewById(R.id.listView);
-    arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
-    listView.setAdapter(arrayAdapter);
-    }
+public class MainActivity {
 }
